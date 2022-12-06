ad8218569c48c0f077dcc44cfa56366ef68d0676
PhilippeJ-code <jacobs.philippe@skynet.be>
2022-12-06 20:31:44 +0100

    create commands

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -71,9 +71,12 @@ function viessmannIot_update()
         config::save('functionality::cronHourly::enable', 0, 'viessmannIot');
     }
 
+	log::add('viessmannIot', 'info', 'Update');
+
     foreach (viessmann::byType('viessmannIot') as $viessmann) {
 		$viessmann->setConfiguration('createCommands', 'Oui')->save(); 
 		$viessmann->save();
+		log::add('viessmannIot', 'info', 'Update create commands');
     }
 
 	$cron = cron::byClassAndFunction('viessmannIot', 'salsa');
